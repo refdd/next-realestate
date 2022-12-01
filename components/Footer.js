d50e67ca2f3cd36d0842bd8b20b04e2fb1bca6c2
refdd <titorefat76@example.com>
2022-12-01 17:46:00 +0200

    try

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import { Box } from "@chakra-ui/react";
+const Footer = ()=>(
+    <Box textAlign="center" p="5" color="gray.600" borderTop="1px" borderColor="gray.400">
+       2022 Realtor , Ins , \REFAT/     
+    </Box>
+);
+export default Footer;
